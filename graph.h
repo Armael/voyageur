1cd65fd9bff2fe2c1dc0240020b54a323416334c
Armael <mail.armael.gueneau@gmail.com>
2012-10-10 00:58:53 +0200

    Correction de l'oubli du graphe en argument pour addEdge.

--- graph.h
+++ graph.h
@@ -30,7 +30,7 @@ Graph* graph_newWithNodes(int nodesNb);
 /* Ajoute une arête entre les nœuds d'indices n1 et n2, et de poids
    weight. Si n1 ou n2 n'est pas dans le graphe, la fonction de fait
    rien. */
-void graph_addEdge(int n1, int n2, int weight);
+void graph_addEdge(Graph* g, int n1, int n2, int weight);
 
 /* Libère la mémoire occupée par le graphe (nœuds et arêtes) */
 Edge* graph_free(Graph* g);
