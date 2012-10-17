dd74d3e697eb474d34a0bfd5d8740b9f8a7a7207
Armael <mail.armael.gueneau@gmail.com>
2012-10-18 00:00:27 +0200

    Ajout de commentaires pour kruskal.c/kruskal.h

--- kruskal.c
+++ kruskal.c
@@ -2,9 +2,13 @@
 #include "graph.h"
 #include "ftree.h"
 
+/* À partir d'un graphe donné, crée une forêt où chaque Ftree est
+   réduit à un nœud, et représente un nœud du graphe. Il est placé au
+   même indice que dans le graphe, et contient cet indice */
 Forest* initForestFromGraph(Graph* g) {
   Forest* f = malloc(sizeof(Forest));
   
+  /* On connait le nombre de nœuds à l'avance, optimisons, donc */
   f->trees = vector_newWithSize(g->nodesNb);
   f->treesNb = g->nodesNb;
 
@@ -16,12 +20,19 @@ Forest* initForestFromGraph(Graph* g) {
   return f;
 }
 
+/* À partir d'un graphe donné et de la forêt retournée par
+   initForestFromGraph(), retourne la liste des arêtes sous forme de
+   Kedges. */
 Kedge* KedgeListFromGraph(Graph* g, Forest* f) {
   Kedge* klist = NULL;
 
   int i;
   for(i=0; i < g->nodesNb; i++) {
     Edge* e = GET_EDGE(g->nodes, i);
+    /* Parcours de la liste chainée des arêtes partant du nœud. On
+     obtient bien des arêtes en double (obtenues depuis a->b et b->a)
+     mais en pratique, l'algorithme de Kruskal n'est pas perturbé par
+     la présence de doublons (ceux-ci sont ignorés) */
     while(e != NULL) {
       Kedge* new = malloc(sizeof(Kedge));
       new->n1 = i;
@@ -39,6 +50,13 @@ Kedge* KedgeListFromGraph(Graph* g, Forest* f) {
   return klist;
 }
 
+/* Coupe une liste d'arêtes de taille n en deux listes de taille
+   n/2. Les listes de Kedges *part1 et *part2 passées en argument par
+   référence sont modifiése pour correspondre aux deux parties
+   obtenues.  Cette fonction n'alloue pas de mémoire : elle est
+   destructive sur la liste passée en argument. Seules les deux
+   parties sont utilisables après l'appel, la liste complète n'est
+   plus accessible */
 void split(Kedge* list, Kedge** part1, Kedge** part2) {
   *part1 = NULL;
   *part2 = NULL;
@@ -46,6 +64,8 @@ void split(Kedge* list, Kedge** part1, Kedge** part2) {
   int i = 0;
   while(list != NULL) {
     Kedge *next = list->next;
+    /* On alterne en écrivant une fois dans *part1, une autre fois
+       dans *part2 */
     if(i%2 == 0) {
       list->next = *part1;
       *part1 = list;
@@ -58,6 +78,9 @@ void split(Kedge* list, Kedge** part1, Kedge** part2) {
   }
 }
 
+/* Fusion de deux listes triées en une liste triée. Ici encore, le
+   processus est destructif : tenter d'accéder à *part1 ou *part2
+   après l'appel à la fonction n'a pas de sens */
 Kedge* merge(Kedge* part1, Kedge* part2) {
   if(part1 == NULL) {
     return part2;
@@ -74,6 +97,7 @@ Kedge* merge(Kedge* part1, Kedge* part2) {
   }
 }
 
+/* Tri fusion, complexité O(n log(n)) */
 Kedge* mergeSort(Kedge* list) {
   if(list == NULL) {
     return list;
--- kruskal.h
+++ kruskal.h
@@ -1,11 +1,20 @@
 #ifndef KRUSKAL_H_INCLUDED
 #define KRUSKAL_H_INCLUDED
 
+/* Type définissant une forêt. Il s'agit simplement d'un Vector
+   contenant des arbres, et de leur nombre */
 typedef struct forest {
   Vector* trees;
   int treesNb;
 } Forest;
 
+/* Arête utilisée par l'algorithme de Kruskal : elle connecte des
+   nœuds de différents Ftrees placés dans une forêt. Elle contient les
+   indices des nœuds, mais aussi les addresses du nœud Ftree
+   correspondant (et également le poids).  Ces arêtes sont stockées
+   sous forme de liste (simplement) chaînée, d'où la présence du
+   pointeur next vers l'élément suivant (la liste vide est représentée
+   par NULL) */
 typedef struct kedge {
   int n1;
   Ftree* addr1;
@@ -18,10 +27,18 @@ typedef struct kedge {
   struct kedge *next;
 } Kedge;
 
-
+/* À partir d'un graphe donné, crée une forêt où chaque Ftree est
+   réduit à un nœud, et représente un nœud du graphe. Il est placé au
+   même indice que dans le graphe, et contient cet indice */
 Forest* initForestFromGraph(Graph* g);
+
+/* À partir d'un graphe donné et de la forêt retournée par
+   initForestFromGraph(), retourne la liste des arêtes sous forme de
+   Kedges. */
 Kedge* KedgeListFromGraph(Graph* g, Forest* f);
 
+/* Fait un tri fusion d'une liste d'arêtes Kedges. Complexité
+   O(n log(n)) */
 Kedge* mergeSort(Kedge* list);
 
 #endif
